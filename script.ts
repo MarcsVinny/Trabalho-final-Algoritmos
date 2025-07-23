b17b1ba7427b92cf132868be0f285a847e79f7ea
MarcsVinny <marcos123vinnycius@gmail.com>
2025-07-22 21:14:15 -0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,2 +1,20 @@
-let pergunta : string;
-pergunta = String(prompt("O que é um algoritmo?)")!);
+//criar o personagem do sobrevivente
+ class sobrevivente{
+    nome : string;
+    vidas : number;
+    errosCometidos : number;
+    status: string;
+
+    constructor(nome: string, vidas: number, errosCometidos: number, status: string){
+        this.nome = nome;
+        this.vidas = vidas;
+        this.errosCometidos = errosCometidos;
+        this.status = status;
+    }
+ }
+
+ perderVida(){
+    thisvidas--;
+    this.errosCometidos++;
+    alert("Você perdeu uma vida!  " + ")
+ }
